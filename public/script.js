33af2029bca3c7b024c8cf918d98efe531412702
Mubashir Reshamwala <mubresham@gmail.com>
2020-01-26 12:33:17 +0530

    BackgroundSync, A2HS

--- public/script.js
+++ public/script.js
@@ -1,13 +1,60 @@
 console.log('script initialized');
-if('serviceWorker' in navigator){
+let deferredPrompt;
+if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
+    let btn = document.getElementById('install');
     navigator.serviceWorker
       .register('../sw.js')
       .then(reg => {
-        console.log('Service worker: Registered!')
+        console.log('Service worker: Registered!');
+        reg.sync.register('mySync');
+        window.addEventListener('beforeinstallprompt', e => {
+          console.log(`Service Worker: beforeinstallprompt`);
+          // Prevent Chrome 76 and later from showing the mini-infobar
+          e.preventDefault();
+          // Stash the event so it can be triggered later.
+          deferredPrompt = e;
+          btn.addEventListener('click', installApp);
+          window.addEventListener('appinstalled', (evt) => {
+            console.log('App installed!');
+          });
+        });
       })
       .catch(e => {
-        console.log(`Service Worker:  Error: ${e}`)
+        console.log(`Service Worker:  Error: ${e}`);
+        getPosts();
       });
+  });
+} else {
+  getPosts();
+}
+let getPosts = () => {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(e => console.error(e));
+};
+function installApp() {
+  console.log('installApp')
+  
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then(choiceResult => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt');
+    } else {
+      console.log('User dismissed the A2HS prompt');
+    }
+    deferredPrompt = null;
+  });
 }
--- sw.js
+++ sw.js
@@ -1,50 +1,102 @@
-let CACHE_VERSION='0.0.2';
-urls = [
-  '/public/',
-  '/public/script.js',
-  '/public/style.css'
-]
+let CACHE_VERSION = '0.0.2';
+urls = ['/public/', '/public/script.js', '/public/style.css'];
 self.addEventListener('install', e => {
   console.log('Service Worker: Installed!');
   e.waitUntil(
-    caches.open(CACHE_VERSION)
+    caches
+      .open(CACHE_VERSION)
       .then(cache => {
         console.log('Service Worker: Cache opened', urls);
         cache.addAll(urls);
       })
       .catch(e => {
-        console.error(e)
-      })
+        console.error(e);
+      }),
+  );
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_VERSION) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
   );
 });
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request)
-      .then(res => {
-        if(res) {
-          console.log('returning cached result');
+    caches.match(e.request).then(res => {
+      if (res) {
+        console.log('returning cached result', e.request);
+        return res;
+      }
+      console.log('sending request to the server');
+      return fetch(e.request)
+        .then(res => {
+          if (!res || res.status !== 200 || res.type === 'basic') {
+            return res;
+          }
+          console.log('New cache time');
+          // Duplicate the stream to cache and pass to the browser
+          var responseToCache = res.clone();
+          caches.open(CACHE_VERSION).then(cache => {
+            console.log('New cache received');
+            cache.put(e.request, responseToCache);
+          });
           return res;
+        })
+        .catch(e => {
+          console.log('New cache failed');
+          console.log(e);
+        });
+    }),
+  );
+});
+
+// Background Sync
+
+self.addEventListener('sync', function(event) {
+  console.log('Service Worker sync:', event);
+  
+  if (event.tag === 'mySync') {
+    console.log('Service Worker sync: ', event);
+    event.waitUntil(async () => {
+      return new Promise((resolve, reject) => {
+        try {
+          fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+              title: 'foo',
+              body: 'bar',
+              userId: 1,
+            }),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+          })
+          .then(data => {
+            return response.json();
+          })
+          .then(data => {
+            console.log(data);
+            resolve(data);
+          })
+          .catch(error => {
+            console.error(error);
+            reject(error);
+          });
+          // return data;
+          
+        } catch (error) {
+          console.error(error);
+          reject(error);
+          // throw error;
         }
-        console.log('sending request to the server');
-        return fetch(e.request)
-          .then(res => {
-            if(!res || res.status !== 200 || res.type === 'basic') {
-              return res;
-            }
-            console.log('New cache time')
-             // Duplicate the stream to cache and pass to the browser
-            var responseToCache = res.clone();
-            caches.open(CACHE_VERSION)
-              .then(cache => {
-                console.log('New cache received')
-                cache.put(e.request, responseToCache);
-              });
-              return res;
-          })
-          .catch(e => {
-            console.log('New cache failed')
-            console.log(e)
-          })
       })
-  )
-})
+    });
+  }
+});
